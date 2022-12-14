a038a52f2da2d2debc47058d28d66235d7084231
Ahmed Selmi <ahmedselmi939@gmail.com>
2022-12-14 23:03:53 +0100

    final

--- Personne.java
+++ Personne.java
@@ -1,46 +1,8 @@
-class Personne{
-   private String nom;
-    private String prenom;
-    private int age;
-    public Personne(String nom, String prenom, int age){
-        this.nom = nom;
-        this.prenom = prenom;
-        this.age = age;
-    }
-    public void afficher(){
-        System.out.println("Nom : " + nom + " Prenom : " + prenom + " Age : " + age);
-    }
+class Bibliothéque{
+    public static void main(String[] args) {
+        Adherant a = new Adherant("Doe", "John", 25, "123456");
+       livre l = new livre(123456, "Le livre",new Auteur("Doe", "John", 25, "123456"));
+       System.out.println( a.afficher());
+       System.out.println(l.afficher1());
+ }
 }
-class Adherant extends Personne{
-    private String numadhérant;
-    public Adherant(String nom, String prenom, int age, String numadhérant)
-    {
-        super(nom, prenom, age);
-        this.numadhérant = numadhérant;
-    }
-    public void afficher(){
-        super.afficher();
-        System.out.println("Numéro d'adhérant : " + numadhérant);
-    }
-    
-
-     }
-     class Auteur extends Personne{
-            private String numAuteur;
-            public Auteur(String nom, String prenom, int age, String numAuteur){
-                super(nom, prenom, age);
-                this.numAuteur = numAuteur;
-            }
-            public void afficher(){
-                super.afficher();
-                System.out.println("Numéro d'auteur : " + numAuteur);
-            }
-            
-     }
-     
-
-  
-
-
-
-
--- main.java
+++
@@ -1,8 +0,0 @@
-public class main {
-   public static void main(String[] args) {
-       Personne p = new Personne("Doe", "John", 25);
-       Adherant a = new Adherant("Doe", "John", 25, "123456");
-       p.afficher();
-       a.afficher();
-   } 
-}
